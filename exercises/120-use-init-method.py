91a674a3e222ff2a00bd11daa560dd7dc1b24fca
etcyber <accounts@engitech.uk>
2022-05-03 07:23:35 +0100

    changing files permissions

--- exercises/120-use-init-method.py
+++ exercises/120-use-init-method.py
@@ -8,10 +8,14 @@ class Cat:
 
 # 1 Instantiate the Cat object with 3 cats
 
-
+peanut = Cat('Peanut', 10)
+garfield = Cat('Garfield', 14)
+snickers = Cat('Snickers', 16)
 
 # 2 Create a function that finds the oldest cat
 
-
+def get_oldest_cat(*args):
+    return max(args)
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f"The oldest cat is {get_oldest_cat(peanut.age, garfield.age, snickers.age)} years old")
