e577369d092619d56f5fc3bd7b88ecd18d71b9c1
cyber-arknet <iulian.pro@proton.me>
2023-08-13 23:47:47 +0100

    Adding test and regex modules

--- 184-debug-code.py
+++ 184-debug-code.py
@@ -1,2 +1,14 @@
 # debugging
 
+#linting
+#ide/ editor
+#read errors
+#pdb is a built in module
+
+import pdb
+def add(num1, num2):
+    pdb.set_trace() # instead of using print (num1, num2)
+    t = 4*5
+    return num1 + num2
+
+add(4, 'hdhdhff')
